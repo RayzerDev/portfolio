36e636b7f5af7614c9a9b6e3acf638d83883b39f
Louis Karamucki <louis_karamucki@ens.univ-artois.fr>
2024-09-14 15:24:46 +0200

    Edition de la page compétences

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,23 +1,30 @@
-import { promises as fs } from 'fs';
-import path from 'path';
-import Image from "next/image";
-import DataSingleton from "@/utils/dataUtils";
+import Image from 'next/image';
+import DataSingleton from '@/utils/dataUtils';
 
 export default async function Projects() {
-
-  const skills = await DataSingleton.getInstance().getSkillsData();
-
-  return (
-      <div>
-        <h1>skills</h1>
-        <ul>
-          {skills?.map((skill: any) => (
-              <li key={skill.id}>
-                  <Image src={"/" + skill.image} alt={skill.nom} width={50} height={50}/>
-                  <p>{skill.nom}</p>
-              </li>
-          ))}
-        </ul>
-      </div>
-  );
+    const groupedSkills = await DataSingleton.getInstance().groupSkillsByCategory();
+    return (
+        <div className="container mx-auto py-12 px-4 md:px-6 lg:px-8">
+            <section className="flex-row mb-12">
+                {Object.entries(groupedSkills).map(([category, skills]) => (
+                    <div key={category} className="flex-col mb-8">
+                        <h2 className="text-3xl font-bold tracking-tight text-foreground mb-2">{category}</h2>
+                        <div className="flex flex-wrap gap-4">
+                            {skills.map((skill: any) => (
+                                <div key={skill.id} className="flex-col text-center">
+                                    <Image
+                                        src={`/${skill.image}`}
+                                        alt={skill.nom}
+                                        width={100}
+                                        height={100}
+                                    />
+                                    <p className="mt-1">{skill.nom}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </section>
+        </div>
+    );
 }
