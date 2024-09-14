87adba3a5f4d3673b6e0af09bb08f09c51c7d3f1
Louis Karamucki <louis_karamucki@ens.univ-artois.fr>
2024-09-14 15:20:50 +0200

    Edition de la page projets

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,33 +1,40 @@
 import Image from "next/image";
 import DataSingleton from "@/utils/dataUtils";
+import {Card, CardContent, CardFooter, CardHeader} from "@/components/ui/card";
 
 export default async function Projects() {
 
     const projects = await DataSingleton.getInstance().getProjectsData();
 
     return (
-        <div>
-            <h1>Projets</h1>
-            <ul>
+        <div className="container mx-auto py-12 px-4 md:px-6 lg:px-8">
+            <section className="mb-12">
+                <h1 className="text-5xl font-bold tracking-tight text-foreground">Projets</h1>
                 {projects?.map((projet: any) => (
-                    <li key={projet.id}>
-                        <Image src={"/" + projet.image} alt={projet.nom} width={50} height={50} />
-                        <h2>{projet.nom}</h2>
-                        <p>{projet.description}</p>
-                        {projet.previewLink && (
-                            <a href={projet.previewLink} target="_blank" rel="noopener noreferrer">
-                                Preview
-                            </a>
-                        )}
-                        {projet.githubLink && (
-                            <a href={projet.githubLink} target="_blank" rel="noopener noreferrer">
-                                GitHub
-                            </a>
-                        )}
-                        <p>Techs: {projet.skills.map((skill: any) => skill.nom).join(', ')}</p>
-                    </li>
+                    <Card key={projet.id}>
+                        <CardHeader>
+                            <Image src={"/" + projet.image} alt={projet.nom} width={500} height={500}/>
+                            <h2>{projet.nom}</h2>
+                        </CardHeader>
+                        <CardContent>
+                            <p>{projet.description}</p>
+                            <p>Techs: {projet.skills.map((skill: any) => skill.nom).join(', ')}</p>
+                        </CardContent>
+                        <CardFooter>
+                            {projet.previewLink && (
+                                <a href={projet.previewLink} target="_blank" rel="noopener noreferrer">
+                                    Preview
+                                </a>
+                            )}
+                            {projet.githubLink && (
+                                <a href={projet.githubLink} target="_blank" rel="noopener noreferrer">
+                                    GitHub
+                                </a>
+                            )}
+                        </CardFooter>
+                    </Card>
                 ))}
-            </ul>
+            </section>
         </div>
     );
 }
