6a17b365185a6048e5473f7b9dd017720d57ca83
RayzerDev <rayzergamer62@outlook.fr>
2024-09-15 13:16:45 +0200

    Correction pour déployer avec github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    basePath: process.env.NODE_ENV === 'production' ? '/nom-utilisateur.github.io' : '',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
